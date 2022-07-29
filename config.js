1a76ce32f52fb26eea3c57798c33e6904482ab13
Borek Bandell <489843@student.fontys.nl>
2022-07-29 16:17:32 +0200

    Created config file

---
+++ config.js
@@ -0,0 +1,11 @@
+export const MazdaWebsites = {
+    nl: 'https://www.mazda.nl/services/handleidingen/',
+    de: 'https://www.mazda.de/service-zubehoer/videoanleitungen-handbuecher/',
+    uk: 'https://www.mazda.co.uk/owners/videos-and-manuals/',
+}
+
+export const EmailConfig = {
+    address : 'borekbandell@gmail.com',
+    password : 'nwylphinruoakmnc',
+    service : 'gmail',
+}
