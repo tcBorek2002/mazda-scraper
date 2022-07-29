c52a6d0524145b06cf8f5031ad87256f9733e909
Borek Bandell <489843@student.fontys.nl>
2022-07-29 16:18:03 +0200

    Scraper file refactoring

--- scraper.js
+++ scraper.js
@@ -1,59 +1,30 @@
-const Nightmare = require('nightmare')
+import Nightmare from 'nightmare';
 const nightmare = Nightmare({ show: false})
-var nodemailer = require('nodemailer');
+import {MazdaWebsites} from './config.js';
 
-console.log('Starting execution');
-nightmare
-	.goto('https://www.mazda.nl/services/handleidingen/')
-    .click('#onetrust-accept-btn-handler')
-	.wait('.dropdown__option-caption-3RkJ1')
-	.evaluate(
-		() =>
-        Array.from(document.querySelectorAll('.dropdown__option-caption-3RkJ1')).map(element => element.innerText)
-	)
-	.end()
-	.then((list) => {
-		let title = 'The Mazda list has been checked, no changes';
+export async function ScrapeWebsite (website) {
+	try {
+		console.log('Starting scraping');
+		const list = await nightmare
+			.goto(website)
+			.click('#onetrust-accept-btn-handler')
+			.wait('.dropdown__option-caption-3RkJ1')
+			.evaluate(
+				() =>
+				Array.from(document.querySelectorAll('.dropdown__option-caption-3RkJ1')).map(element => element.innerText)
+			)
+			.end();
+			// list.forEach((car) => {
+			// 	if(car.includes('60')) {
+			// 		title = 'Mazda CX-60 has been spotted in the car list!'
+			// 	}
+			// })
 
-		if(list.length !== 8) {
-			console.log('The Mazda car list has changed!');
-			title = 'The Mazda manual list has changed!';
-		}
-		else {
-			console.log('The car list still appears the same.');
-		}
-		list.forEach((car) => {
-			if(car.includes('60')) {
-				title = 'Mazda CX-60 has been spotted in the car list!'
-			}
-		})
-
-		console.log('Car list:', list);
-
-		var transporter = nodemailer.createTransport({
-			service: 'gmail',
-			auth: {
-			  user: 'borekbandell@gmail.com',
-			  pass: 'nwylphinruoakmnc'
-			}
-		  });
-		  
-		  var mailOptions = {
-			from: 'borekbandell@gmail.com',
-			to: 'borekbandell@gmail.com',
-			subject: title,
-			text: 'This is the list: ' + list.toString()
-		  };
-		  
-		  transporter.sendMail(mailOptions, function(error, info){
-			if (error) {
-			  console.log(error);
-			} else {
-			  console.log('Email sent: ' + info.response);
-			}
-		  });
-
-	})
-	.catch((error) => {
-		console.error('Search failed:', error)
-	})
+			console.log('Car list:', list);
+			return (list);
+	}
+	catch (e) {
+		console.error(e);
+		return [];
+	}
+}
