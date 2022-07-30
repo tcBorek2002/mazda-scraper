82d5336086a6dee66c99eb0d8d3364bbd390b4a4
Borek Bandell <489843@student.fontys.nl>
2022-07-30 21:18:56 +0200

    Fixed bug preventing multiple Nightmare instances

--- scraper.js
+++ scraper.js
@@ -2,10 +2,12 @@ import Nightmare from 'nightmare';
 const nightmare = Nightmare({ show: false})
 import {MazdaWebsites} from './config.js';
 
-export async function ScrapeWebsite (website) {
+export async function ScrapeWebsite (website, shouldEnd) {
+	let list = [];
 	try {
-		console.log('Starting scraping');
-		const list = await nightmare
+		console.log('Starting scraping: ', website);
+		if(shouldEnd) {
+			list = await nightmare
 			.goto(website)
 			.click('#onetrust-accept-btn-handler')
 			.wait('.dropdown__option-caption-3RkJ1')
@@ -14,6 +16,17 @@ export async function ScrapeWebsite (website) {
 				Array.from(document.querySelectorAll('.dropdown__option-caption-3RkJ1')).map(element => element.innerText)
 			)
 			.end();
+		}
+		else {
+			list = await nightmare
+				.goto(website)
+				.click('#onetrust-accept-btn-handler')
+				.wait('.dropdown__option-caption-3RkJ1')
+				.evaluate(
+					() =>
+					Array.from(document.querySelectorAll('.dropdown__option-caption-3RkJ1')).map(element => element.innerText)
+			);
+		}
 			// list.forEach((car) => {
 			// 	if(car.includes('60')) {
 			// 		title = 'Mazda CX-60 has been spotted in the car list!'
